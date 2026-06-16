ebac1c3294d71e47438f9c32c0fe39c1e770ea46
xiaochao-66 <724940889@qq.com>
2026-06-16 20:23:21 +0800

    Deploy sw.js

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE = 'shanka-v4';
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(['./']))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
